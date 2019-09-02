47b4ad01e5968c95eb6ada79ea1cf46c27e3246f
Ghoul <rodrigueznatalandy@gmail.com>
2019-09-02 17:42:41 -0400

    Added base code for Lab 2 waitpid part

---
+++ Lab-2/process-forking.c
@@ -0,0 +1,54 @@
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void child_process(int pid, int parent_pid)
+{
+    printf("Child PID: %d \nParent PID: %d\n\n", pid, parent_pid);
+}
+
+
+void main()
+{
+    int pid;
+    pid = fork();
+
+    if (pid < 0)
+     {
+        fprintf(stderr, "Fork failed!\n");
+        exit(-1);
+    }
+
+    else if (pid==0)
+     {
+
+        execlp("/bin/ps", "ps", NULL);
+
+        printf("Still around...\n");
+    }
+
+    else 
+    {
+        child_process(pid,(int) getpid());
+        int wait_pid = waitpid(pid);
+
+        if( wait_pid < 0)
+        {
+            printf("Something went wrong while waiting for child!\n");
+        }
+
+        else
+        {
+            // printf("Executed successfully\n");
+            printf("Child's PID: %d\n\n", pid);
+
+        }
+        //Wait for the child process to finish before continuing
+        exit(0);
+    }
+
+}
+
+
