62a7ac7bd9305de711e572ec55940a6d97d3c1a6
Ghoul <rodrigueznatalandy@gmail.com>
2019-09-07 18:06:15 -0400

    Added the functionality to our shell

--- Lab-3/shell.c
+++ Lab-3/shell.c
@@ -7,6 +7,8 @@
 #define TOKEN_DELIMETERS " \t\r\n\a"
 #define DEFAULT_SIZE 1024
 
+void child_process(int pid, int parent_pid, char* cmd);
+
 char *history[DEFAULT_SIZE];
 char *argv[DEFAULT_SIZE];
 int hist_count = 0;
@@ -39,17 +41,22 @@ void parse_line(char *line,char *argv[]){
 }
 
 int nat_history(char *argv[]){
-	if(position==2){
-            	hist_num = atoi(argv[1]);
-            	for(int k = 1; k<=hist_count;k++){
-                	if(hist_num == k){
-                		isHistNum = 1;
+	if(position==2)
+	{
+        hist_num = atoi(argv[1]);
+		for(int k = 1; k<=hist_count;k++)
+		{
+			if(hist_num == k)
+			{
+				isHistNum = 1;
 			}
 		}
-        }		
-        if(isHistNum==0){
-                for(int i =0; i <hist_count;i++)
-                    	printf(" %d %s\n",(i+1),history[i]);
+    }		
+
+    if(isHistNum==0)
+	{
+		for(int i =0; i <hist_count;i++)
+				printf(" %d %s\n",(i+1),history[i]);
 	}
 	return 1;
 }
@@ -57,30 +64,126 @@ int nat_history(char *argv[]){
 void execute(char *argv[]){
 	
 	// Check if command is valid as a native command
-	char exit[10];	strcpy(exit, "exit");
+	char exits[10];	strcpy(exits, "exit");
 	char hist[10];	strcpy(hist, "history");
+	char help[10]; 	strcpy(help, "help");
 	
-	if(strcmp(argv[0], exit) == 0){
+	if(strcmp(argv[0], exits) == 0){
 		exit(0);
 	}else if(strcmp(argv[0], hist) == 0){
 		nat_history(argv);
 	}
+	else if(strcmp(argv[0], help) ==0){
+		printf("To use this commandline simple type a valid command and hit 'enter'\n");
+		printf("help: displays a message explaining how to use the CLI\n");
+		printf("exit: closes the CLI\n");
+		printf("cd: executes the cd (change directory) command, your shell should manage it natively\nhistory: without parameters, the history command shows a list of all executed commands:\n\n");
+
+	}
 		//Native commands
-	else{	
-		//Execute any other command. Uses child process. 
+	else
+	{	
+		//Execute any other command. Uses child process.
+		int pid;
+		pid = fork();
+
+		if (pid < 0)
+		{
+			fprintf(stderr, "Fork failed!\n");
+			exit(-1);
+		}
+
+		else if (pid==0)
+		{
+			char *const parmList[] = {"/bin/ls", "-l", "/u/userid/dirname", NULL};
+			printf("\n");
+			// execv(argv[0], parmList);
+			child_process(pid, pid, argv[0]);
+			printf("\n\n");
+			exit(0);
+			printf("Still around...\n\n");
+		}
+
+		else 
+		{
+			//child_process(pid,(int) getpid(), argv[0]);
+			int wait_pid = waitpid(pid);
+
+			if( wait_pid < 0)
+			{
+				// printf("Something went wrong while waiting for child!\n");
+				exit(-1);
+			}
+
+			else
+			{
+				// printf("\n\nExecuted Parent successfully\n");
+				// printf("Child's PID: %d\n\n", pid);
+			// 	char *const parmList[] = {"/bin/ls", "-l", "/u/userid/dirname", NULL};
+			// printf("\n");
+			// execv(argv[0], parmList);
+			// printf("\n\n");
+
+			}
+			//Wait for the child process to finish before continuing
+			//exit(0);
+		}
+
+
 	}		
+}
+
+char * get_directory_of(char* cmd, char * cdir)
+{
+	int size = (sizeof(cmd) / sizeof(char));
+	cdir = (char *) malloc(size+6 * sizeof(char)) ;// /bin/
+
+	for (int i=0 ; i < size-5 ; i++)
+	{
+		cdir[i+5] =  cmd[i];
+	}
+	*cdir = '/';
+	*(cdir+1) = 'b';
+	*(cdir+2)= 'i';
+	*(cdir+3)= 'n';
+	*(cdir+4)= '/';
+
+	*(cdir+size-1)= '\0';
+
+	for (int i=0 ; i < size; i++)
+	{
+		printf("%c",cdir[i]);
+	}
+	printf("\n");
+	return cdir;
+
+}
+
+void child_process(int pid, int parent_pid, char* cmd)
+{
+	// char * direct_to_exec;
+	// get_directory_of(cmd, direct_to_exec);
+	// char *const parmList[] = {"/bin/ls", "-l", NULL};
+    //printf("Child PID: %d \nParent PID: %d\n\n", pid, parent_pid);
+	// execl(direct_to_exec, cmd,(char *) NULL);
+	system(cmd);
+	exit(0);
 }
 
 int main(int argc, char *argv[]){
 	int valid = 0;
-    	char *line = (char*)malloc(DEFAULT_SIZE);
-    	for(int i = 0;i<DEFAULT_SIZE;i++)
-        	history[i] = (char*)malloc(DEFAULT_SIZE);
-        long size;
-        char *buf;
-        char *ptr;
-	while(1){
-		printf("Shell->");
+	char *line = (char*)malloc(DEFAULT_SIZE);
+
+	for(int i = 0;i<DEFAULT_SIZE;i++)
+		history[i] = (char*)malloc(DEFAULT_SIZE);
+
+	long size;
+	char *buf;
+	char *ptr;
+
+	while(1)
+	{
+		printf("What Will Pika Do?->");
 		read_line(line);
 		parse_line(line,argv);
 		execute(argv);
@@ -88,4 +191,7 @@ int main(int argc, char *argv[]){
         		argv[j] = NULL;
 		}	
 	}
+
+	free(history);
+	free(line);
 }
